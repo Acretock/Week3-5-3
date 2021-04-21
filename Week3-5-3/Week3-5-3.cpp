6191e18702da2fa9a03e5248e9577b2f09e804b0
Acretock <georgoncharov@gmail.com>
2021-04-21 17:56:47 +0500

    v1.0

--- Week3-5-3/Week3-5-3.cpp
+++ Week3-5-3/Week3-5-3.cpp
@@ -1,20 +1,53 @@
-// Week3-5-3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <vector>
 #include <iostream>
+
+using namespace std;
+
+class Incognizable
+{
+public:
+	Incognizable();
+	Incognizable(int a);
+	Incognizable(int a, int b);
+	~Incognizable();
+
+private:
+	int first;
+	int second;
+};
+
+Incognizable::Incognizable()
+{
+}
+
+
+Incognizable::Incognizable(int a)
+{
+	first = a;
+	second = 0;
+}
+
+
+Incognizable::Incognizable(int a, int b)
+{
+	first = a;
+	second = b;
+}
+
+
+
+
+
+Incognizable::~Incognizable()
+{
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Incognizable a;
+	Incognizable b = {};
+	Incognizable c = { 0 };
+	Incognizable d = { 0,1 };
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
